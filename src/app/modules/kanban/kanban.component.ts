9f6d11212ae56b9bcc97ee88f917ae6c5ba9dc6e
Tiago <tiagobarbosa02@outlook.com>
2021-10-10 12:06:42 -0300

    Organizando o quadro kanban

--- src/app/modules/kanban/kanban.component.ts
+++ src/app/modules/kanban/kanban.component.ts
@@ -9,12 +9,7 @@ import { Column } from './models/columns.model';
   styleUrls: ['./kanban.component.scss']
 })
 export class KanbanComponent implements OnInit {
-  columns: any = [
-    {id: 1, title: 'To Do', cards: [{title: 'A Fazer A Fazer A Fazer A Fazer A Fazer A Fazer A Fazer A Fazer', description: 'AAAAAAAA', status: 'red'}, {title: 'A Fazer', description: 'AAAAAAAA', status: 'blue'}]},
-    {id: 2, title: 'In Progres', cards: [{}]},
-    {id: 3, title: 'Done', cards: [{title: 'Terminados', description: 'AAAAAAAA', status: 'red'}, {title: 'Terminados5', description: 'AAAAAAAA', status: 'green'}, {title: 'Terminados'}, {title: 'Terminados', description: 'AAAAAAAA',}, {title: 'Terminados'}, {title: 'Terminados', description: 'AAAAAAAA',}, {title: 'Terminados5'}, {title: 'Terminados'}, {title: 'Terminados'}, {title: 'Terminados'}, {title: 'Terminados5'}, {title: 'Terminados'}, {title: 'Terminados'}, ]},
-    {id: 4, title: 'Test', cards: [{}]}
-  ]
+  showInput: boolean = false
 
   board: Board = new Board("Test Board", [
     new Column('Ideas', [
@@ -49,21 +44,6 @@ export class KanbanComponent implements OnInit {
     
   }
 
-  todo = [
-    'Get to work',
-    'Pick up groceries',
-    'Go home',
-    'Fall asleep'
-  ];
-
-  done = [
-    'Get up',
-    'Brush teeth',
-    'Take a shower',
-    'Check e-mail',
-    'Walk dog'
-  ];
-
   drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
@@ -73,6 +53,10 @@ export class KanbanComponent implements OnInit {
                         event.previousIndex,
                         event.currentIndex);
     }
+  }
+
+  newList() {
+    this.showInput = !this.showInput
   }
 
 }
