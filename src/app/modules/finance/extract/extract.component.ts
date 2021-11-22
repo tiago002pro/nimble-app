fdf7c3e34bdf0568470de372783a680e292fa885
Tiago barbosa <tiagobarbosa02@outlook.com>
2021-11-22 14:35:30 -0300

    Ajuste nos resumos da tela de extrato

--- src/app/modules/finance/extract/extract.component.ts
+++ src/app/modules/finance/extract/extract.component.ts
@@ -6,25 +6,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./extract.component.scss']
 })
 export class ExtractComponent implements OnInit {
-  summary!: Array<any>
+  summary: Array<any> = [{show: Boolean}, {item: Array<any>()}]
   account!: Array<any>
 
   constructor() { }
 
   ngOnInit(): void {
     this.summary = [
-      {name: "Saldo anterior", value: -200},
-      {name: "Entradas", value: 1000},
-      {name: "Saídas", value: -600},
-      {name: "Saldo do período", value: 200},
-      {name: "Saldo atual", value: 200},
-    ]
-
-    this.account = [
-      {name: "Carteira", url: "https://w7.pngwing.com/pngs/528/635/png-transparent-wallet-money-computer-icons-wallet-angle-logo-cash.png", value: 1000},
-      {name: "Itaú", url: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8a/Banco_Ita%C3%BA_logo.svg/1011px-Banco_Ita%C3%BA_logo.svg.png", value: 546},
-      {name: "Santander", url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRVh7c9jvGncG8cNXawbyzmmQCScWxdmGGzKfOUjuY1x-Inyf6sVSXSliRw6wc54ECweQ&usqp=CAU", value: -569},
+      {
+        id: 'balance',
+        show: true, 
+        item: [
+          {name: "Saldo anterior", value: -200},
+          {name: "Entradas", value: 1000},
+          {name: "Saídas", value: -600},
+          {name: "Saldo do período", value: 200},
+          {name: "Saldo atual", value: 200},
+        ]
+      },
+      {
+        id: 'account',
+        show: true,
+        item: [
+          {name: "Carteira", url: "https://firebasestorage.googleapis.com/v0/b/nimble-34cd9.appspot.com/o/Carteira.png?alt=media&token=78c29540-7dac-4111-833f-40c4dff7c8c3", value: 1000},
+          {name: "Itaú", url: "https://firebasestorage.googleapis.com/v0/b/nimble-34cd9.appspot.com/o/Ita%C3%BA.png?alt=media&token=c4ae695f-97c8-4cd0-b787-00aaf60fbb8d", value: 546},
+          {name: "Santander", url: "https://firebasestorage.googleapis.com/v0/b/nimble-34cd9.appspot.com/o/Santander.png?alt=media&token=987c62a6-0753-4830-bb55-b5db482aba9c", value: -569},
+        ]
+      }
     ]
   }
 
+  showSummary(id: String) {
+    this.summary.forEach(item => {
+      console.log(item.id);
+      if (item.id == id) {
+        item.show = !item.show
+      }
+    })
+  }
 }
