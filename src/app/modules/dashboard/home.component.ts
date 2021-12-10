1d6c9cef1b579ea02d160464b8c0b158bde5d54a
Tiago barbosa <tiagobarbosa02@outlook.com>
2021-12-09 23:53:56 -0300

    Comentando o loader component

--- src/app/modules/dashboard/home.component.ts
+++ src/app/modules/dashboard/home.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { NgxUiLoaderService } from 'ngx-ui-loader';
 
 @Component({
   selector: 'app-home',
@@ -7,14 +6,8 @@ import { NgxUiLoaderService } from 'ngx-ui-loader';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent{
-  constructor(
-    private ngxService: NgxUiLoaderService
-  ) {}
+  constructor() {}
 
   ngOnInit(): void {
-    // this.ngxService.start()
-    // setTimeout(() => {
-    //   this.ngxService.stop()
-    // }, 2000)
   }
 }
