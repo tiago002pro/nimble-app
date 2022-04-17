0b6ecaac382c1d39e77c17b0978cb75cf79e456f
Tiago barbosa <tiagobarbosa02@outlook.com>
2022-04-17 09:19:56 -0300

    getcategoties

--- src/app/modules/finance/service/finance.service.ts
+++ src/app/modules/finance/service/finance.service.ts
@@ -1,14 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Pageable } from 'src/app/model/pageable.model';
+import { CategoryTitle } from '../interface/category.interface';
 import { FinanceTitle } from '../interface/title.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FinanceService {
-  url: String = 'https://nimble-back.herokuapp.com' 
-  // url: String = 'http://localhost:8080'
+  // url: String = 'https://nimble-back.herokuapp.com' 
+  url: String = 'http://localhost:8080'
 
   constructor(
     private http: HttpClient,
@@ -20,5 +21,9 @@ export class FinanceService {
 
   getTitlesByType(type: String, page: number, size: number)  {
     return this.http.get<Pageable>((this.url) + '/api/title/history?type='+(type)+'&page='+(page)+'&size='+(size)) 
+  }
+
+  getAllCategories() {
+    return this.http.get<Array<CategoryTitle>>((this.url) + '/api/category/all')
   }
 }
