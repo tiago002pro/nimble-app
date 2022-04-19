3d78428de5a6b037bf143cee3bf95c8b911d5525
Tiago barbosa <tiagobarbosa02@outlook.com>
2022-04-19 14:05:28 -0300

    Ajuestes no routing

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -12,33 +12,13 @@ export class SidebarComponent implements OnInit {
 
   ngOnInit(): void {
     this.sidebarItem = [
-      // {
-      //   name: "Dashboard",
-      //   icon: "bi bi-grid-1x2-fill",
-      //   router: "",
-      //   drop: false,
-      //   sub: []
-      // },
-      // {
-      //   name: "Cadastross",
-      //   icon: "bi bi-person-fill",
-      //   router: "manage-person",
-      //   drop: false,
-      //   sub: []
-      // },
-      // {
-      //   name: "Lançamentoss",
-      //   icon: "bi bi-person-fill",
-      //   router: "finance-releases",
-      //   drop: false,
-      //   sub: []
-      // },
       {
         name: "Cadastros",
         icon: "bi bi-plus-circle",
         router: "#",
         drop: false,
         sub: [
+          {name: "Cadastros", router: "/manage-person"},
           {name: "Cliente", router: "/person-list/client/Clientes"},
           {name: "Fornecedor", router: "/person-list/provider/Fornecedores"},
           {name: "Funcionário", router: "/person-list/employee/Funcionários"},
@@ -51,10 +31,10 @@ export class SidebarComponent implements OnInit {
         drop: false,
         sub: [
           {name: "Lançamentos", router: "finance-releases"},
-          {name: "Contas bancárias", router: "accounts"},
+          {name: "Contas bancárias", router: "account-list"},
           {name: "Contas a Pagar", router: "finance-history/pay/pagar"},
           {name: "Contas a Receber", router: "finance-history/receive/receber"},
-          {name: "Extrato", router: "extract"},
+          {name: "Extrato", router: "account-extract"},
         ]
       },
       {
@@ -66,15 +46,15 @@ export class SidebarComponent implements OnInit {
           {name: "Quadro", router: "/kanban"},
         ]
       },
-      {
-        name: "Relatórios",
-        icon: "fas fa-chart-pie",
-        router: "#",
-        drop: false,
-        sub: [
-          {name: "Quadro", router: "/report"},
-        ]
-      },
+      // {
+      //   name: "Relatórios",
+      //   icon: "fas fa-chart-pie",
+      //   router: "#",
+      //   drop: false,
+      //   sub: [
+      //     {name: "Relatórios", router: "/report"},
+      //   ]
+      // },
     ]
   }
 
