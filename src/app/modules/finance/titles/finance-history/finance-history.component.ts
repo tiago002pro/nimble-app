7fe80856dba9d78f35eeefb0df574659bdf8274c
Tiago barbosa <tiagobarbosa02@outlook.com>
2022-04-25 12:53:51 -0300

    Checkbox do title-list

--- src/app/modules/finance/titles/finance-history/finance-history.component.ts
+++ src/app/modules/finance/titles/finance-history/finance-history.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Pageable } from 'src/app/model/pageable.model';
 import { EnumTitleType } from '../enum/EnumTitleType';
@@ -16,13 +17,18 @@ export class FinanceHistoryComponent implements OnInit {
   type!: string
   titles!: Array<FinanceTitle>
   totalPages!: any
+  form: FormGroup
 
   constructor(
     private route: ActivatedRoute,
-    private financeService: FinanceService
+    private financeService: FinanceService,
+    fb: FormBuilder
   ) {
     this.currentRoute = this.route.snapshot.params.rule
     this.type = this.route.snapshot.params.rule === 'pagar'? EnumTitleType.PAY : EnumTitleType.RECEIVE
+    this.form = fb.group({
+      selectedTitles:  new FormArray([])
+    });
   }
 
   ngOnInit() {
@@ -35,14 +41,15 @@ export class FinanceHistoryComponent implements OnInit {
     this.totalPages = Array(this.list.totalPages).map((x,i)=>i);
   }
 
-  onChange(value, title) {
-    title.selected = !title.selected
-    console.log("value", value);
-    console.log("title", title);
-    console.log("index");
-    this.titles.forEach(title => {
-      // title.selected = false
-    });
+  onChange(event: any) {
+    const selectedTitles = (this.form.controls.selectedTitles as FormArray);
     
+    if (event.target.checked) {
+      selectedTitles.push(new FormControl(event.target.value));
+    } else {
+      const index = selectedTitles.controls
+      .findIndex(x => x.value === event.target.id);
+      selectedTitles.removeAt(index);
+    }
   }
 }
